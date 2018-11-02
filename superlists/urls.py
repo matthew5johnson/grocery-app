cdc1f8401074883453d5e3ee5828d4db7cf09587
matthew5johnson <matthew5johnson@protonmail.com>
2018-11-02 13:21:03 -0500

    new url and view for adding to existing lists. FT and UT are both passing

--- superlists/urls.py
+++ superlists/urls.py
@@ -19,5 +19,12 @@ from lists import views
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
     url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),  # lists/the-only-list-in-the-world/$
+    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
 ]
+
+## the (.+) captures all of the text between the two /  
+## the captured text will get passed to the view as an argument
+## BUT (.+) is a "greedy" regular expression
+## it captures 1/add_item from lists/1/add_item/, but django is guessing that we want a trailing slash and cutting off the end
+## fix this by specifying a number be found in the regex with \d+
