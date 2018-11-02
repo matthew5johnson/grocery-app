6f5ea4d4df5b49f4696522c88db1eecb27e53f63
matthew5johnson <matthew5johnson@protonmail.com>
2018-11-02 10:43:56 -0500

    new lists view added

--- lists/tests.py
+++ lists/tests.py
@@ -29,25 +29,25 @@ class HomePageTest(TestCase):
 
 		self.assertTemplateUsed(response, 'home.html')
 
-	def test_can_save_a_POST_request(self):
-		response = self.client.post('/', data={'item_text': 'A new list item'})
+	# def test_can_save_a_POST_request(self):
+	# 	response = self.client.post('/', data={'item_text': 'A new list item'})
 		
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new list item')
+	# 	self.assertEqual(Item.objects.count(), 1)
+	# 	new_item = Item.objects.first()
+	# 	self.assertEqual(new_item.text, 'A new list item')
 
-		# self.assertIn('A new list item', response.content.decode())
-		# self.assertTemplateUsed(response, 'home.html')
-		### The commented out above was expected a response to the POST request to return the content. Now we want the response to be a redirect to the homepage
+	# 	# self.assertIn('A new list item', response.content.decode())
+	# 	# self.assertTemplateUsed(response, 'home.html')
+	# 	### The commented out above was expected a response to the POST request to return the content. Now we want the response to be a redirect to the homepage
 
-	def test_redirects_after_POST(self):
-		response = self.client.post('/', data={'item_text': 'A new list item'})
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+	# def test_redirects_after_POST(self):
+	# 	response = self.client.post('/', data={'item_text': 'A new list item'})
+	# 	self.assertEqual(response.status_code, 302)
+	# 	self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
-	def test_only_saves_items_when_necessary(self):
-		self.client.get('/')
-		self.assertEqual(Item.objects.count(), 0)
+	# def test_only_saves_items_when_necessary(self):
+	# 	self.client.get('/')
+	# 	self.assertEqual(Item.objects.count(), 0)
 
 	# def test_displays_all_list_items(self):
 	# 	Item.objects.create(text='itemey 1')
@@ -96,4 +96,22 @@ class ListViewTest(TestCase):
 		self.assertContains(response, 'itemey 1')
 		self.assertContains(response, 'itemey 2')
 
+class NewListTest(TestCase):
+
+	def test_can_save_a_POST_request(self):
+		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+		
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+		# self.assertIn('A new list item', response.content.decode())
+		# self.assertTemplateUsed(response, 'home.html')
+		### The commented out above was expected a response to the POST request to return the content. Now we want the response to be a redirect to the homepage
+
+	def test_redirects_after_POST(self):
+		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+		# self.assertEqual(response.status_code, 302)
+		# self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
